91e1647c3feda43b9ee4c2b6bfc8c348050d81fb
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2020-03-09 23:34:44 +0200

    Create main.py

---
+++ BinarySearchTree/Python/main.py
@@ -0,0 +1,17 @@
+from BinarySearchTree import BTS
+
+Root = BTS()
+
+Root.Insert(50)
+Root.Insert(30)
+Root.Insert(70)
+Root.Insert(20)
+Root.Insert(40)
+Root.Insert(60)
+Root.Insert(80)
+
+Root.Delete(20)
+Root.Delete(30)
+Root.Delete(50)
+
+Root.Inorder()
