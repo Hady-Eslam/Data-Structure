9626d40945790bcdce888f29c8ea2738fe47e76c
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2020-03-09 18:11:23 +0200

    Create main.py

---
+++ Stack/Python/main.py
@@ -0,0 +1,13 @@
+from Stack import Stack
+
+The_Stack = Stack()
+
+The_Stack.Push('Hello')
+The_Stack.Push('World')
+The_Stack.Push('Hady')
+
+print(The_Stack.Top())
+
+print(The_Stack.Pop())
+print(The_Stack.Pop())
+print(The_Stack.Pop())
